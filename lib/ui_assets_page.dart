6ff33ff3846235a96e84c1aebf6ca6c4d85b14f6
Tính Phan <tinhpt.38@gmail.com>
2021-05-10 15:41:12 +0700

    make it better

---
+++ lib/ui_assets_page.dart
@@ -0,0 +1,8 @@
+class UIData {
+  static const String imageDir = 'assets/images';
+
+  static const String background = '$imageDir/shape_background.png';
+
+  static const String catUrl =
+      'https://dl.dropboxusercontent.com/s/rdveiep7gf76tft/cat.jpeg?dl=0';
+}
---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+Color randomColor() {
+  Random random = Random();
+
+  return Color.fromARGB(random.nextInt(100), random.nextInt(256),
+      random.nextInt(256), random.nextInt(256));
+}
